d69f95adc84a275175a5ea4bcb84105059ea7ac1
Aurelied <280120@supinfo.com>
2020-05-06 18:22:15 +0200

    setup and example with chart.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+// viewed at http://localhost:8080/
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/data", function (req, res) {
+  res.sendFile(path.join(__dirname + "/Covid-19-json.json"));
+});
+
+app.listen(8080, () => {
+  console.log("Routes.js running !");
+});
