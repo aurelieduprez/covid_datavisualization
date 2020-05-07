f6a0fbc616aacff3431b622213c412635657dee6
Aurelied <280120@supinfo.com>
2020-05-07 14:33:11 +0200

    request for getting data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+const jsonQuery = require("json-query");
 
 // viewed at http://localhost:8080/
 app.get("/", function (req, res) {
@@ -9,6 +10,17 @@ app.get("/", function (req, res) {
 
 app.get("/data", function (req, res) {
   res.sendFile(path.join(__dirname + "/Covid-19-json.json"));
+});
+
+app.get("/franceCases", function (req, res) {
+  var data = require("./Covid-19-json.json");
+  var query = jsonQuery(
+    "records[*countriesAndTerritories=Afghanistan & day = 11]",
+    {
+      data: data,
+    }
+  ); //=> {value: 'Matt', parents: [...], key: 0} ... etc
+  res.send(query);
 });
 
 app.listen(8080, () => {
