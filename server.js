e575e4ecdd3df3a0c0e5d50004b1ae81a954dbbe
Aurelied <280120@supinfo.com>
2020-05-07 22:23:17 +0200

    first chart ok + some css

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var path = require("path");
 const jsonQuery = require("json-query");
 
+app.use("/style", express.static("./style/"));
+
 // viewed at http://localhost:8080/
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
@@ -14,12 +16,17 @@ app.get("/data", function (req, res) {
 
 app.get("/franceCases", function (req, res) {
   var data = require("./Covid-19-json.json");
-  var query = jsonQuery(
-    "records[*countriesAndTerritories=Afghanistan & day = 11]",
-    {
-      data: data,
-    }
-  ); //=> {value: 'Matt', parents: [...], key: 0} ... etc
+  var query = jsonQuery("records[*countriesAndTerritories=France].cases", {
+    data: data,
+  }).value; //=> {value: 'Matt', parents: [...], key: 0} ... etc
+  res.send(query);
+});
+
+app.get("/worldCases", function (req, res) {
+  var data = require("./Covid-19-json.json");
+  var query = jsonQuery("records.cases", {
+    data: data,
+  }).value; //=> {value: 'Matt', parents: [...], key: 0} ... etc
   res.send(query);
 });
 
