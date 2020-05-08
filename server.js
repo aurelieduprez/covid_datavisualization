61f14237756a4a070d50e964e290a28afb50ff20
Aurelied <280120@supinfo.com>
2020-05-08 23:24:10 +0200

    line chart

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/data", function (req, res) {
   res.sendFile(path.join(__dirname + "/Covid-19-json.json"));
 });
 
-app.get("/franceCases", function (req, res) {
+app.get("/franceCasesMarch", function (req, res) {
   var data = require("./Covid-19-json.json");
   var query = jsonQuery(
     "records[*countriesAndTerritories=France & dateRep=30/03/2020].cases",
@@ -25,7 +25,7 @@ app.get("/franceCases", function (req, res) {
   res.send(query);
 });
 
-app.get("/worldCases", function (req, res) {
+app.get("/worldCasesMarch", function (req, res) {
   var data = require("./Covid-19-json.json");
   var query = jsonQuery("records[*dateRep=30/03/2020].cases", {
     data: data,
@@ -33,7 +33,7 @@ app.get("/worldCases", function (req, res) {
   res.send(query);
 });
 
-app.get("/franceDeaths", function (req, res) {
+app.get("/franceDeathsMarch", function (req, res) {
   var data = require("./Covid-19-json.json");
   var query = jsonQuery(
     "records[*countriesAndTerritories=France & dateRep=30/03/2020].deaths",
@@ -44,9 +44,25 @@ app.get("/franceDeaths", function (req, res) {
   res.send(query);
 });
 
-app.get("/worldDeaths", function (req, res) {
+app.get("/worldDeathsMarch", function (req, res) {
   var data = require("./Covid-19-json.json");
   var query = jsonQuery("records[*dateRep=30/03/2020].deaths", {
+    data: data,
+  }).value; //=> {value: 'Matt', parents: [...], key: 0} ... etc
+  res.send(query);
+});
+
+app.get("/getDaysFrance", function (req, res) {
+  var data = require("./Covid-19-json.json");
+  var query = jsonQuery("records[*countriesAndTerritories=France].dateRep", {
+    data: data,
+  }).value;
+  res.send(query);
+});
+
+app.get("/franceCases", function (req, res) {
+  var data = require("./Covid-19-json.json");
+  var query = jsonQuery("records.[*countriesAndTerritories=France].cases", {
     data: data,
   }).value; //=> {value: 'Matt', parents: [...], key: 0} ... etc
   res.send(query);
